3e4f886d22766a26d9763935680cbc15a4e2cba5
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-02-02 22:56:50 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (50.35%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> st;
+        for(char c:s)
+        {
+            if(c=='(' || c=='[' || c=='{')
+            {
+                st.push(c);
+            }
+            else if(!st.empty())
+            {
+                if(c==')')
+                {
+                    if (st.top()=='(')
+                        st.pop();
+                    else
+                        return false;
+                }
+                else if(c==']')
+                {
+                    if(st.top()=='[')
+                    {
+                        st.pop();
+                    }
+                    else
+                        return false;
+                }
+                else if(c=='}')
+                {
+                    if(st.top()=='{')
+                        st.pop();
+                    else
+                        return false;
+                }
+                else return false;
+                
+            }
+            
+            else return false;
+        }
+        if(st.empty())
+            return true;
+        return false;
+        
+    }
+};
