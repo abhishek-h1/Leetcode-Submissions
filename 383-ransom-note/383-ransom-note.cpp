189ed77fa29f73036f0aaae250e52e4749a33b64
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-02-14 23:35:41 +0530

    Time: 11 ms (92.60%), Space: 8.8 MB (75.07%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int a1[256]={0};
-        for(int i=0;i<magazine.length();i++)
+        int n1=ransomNote.length(), n2=magazine.length();
+        if(n1>n2)
+            return false;
+        
+        int a[256]={0};
+        int j=0;
+        for(int i=0;i<n2;i++)
         {
-            a1[magazine[i]]++;
-        }
-        for(int i=0;i<ransomNote.length();i++)
-        {
-            if(a1[ransomNote[i]]>0)
+            a[magazine[i]]++;
+            if(j<n1)
             {
-                a1[ransomNote[i]]--;
+                a[ransomNote[j]]--;
+                j++;
             }
-            else
-                return false;
             
         }
-        return  true;
-        
+        for(int i=0;i<256;i++)
+        {
+            if(a[i]<0)
+                return false;
+        }
+        return true;
     }
 };
