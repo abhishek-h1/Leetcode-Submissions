65084c32c98d7e2d55a3a355163c5dc730b38e03
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-02-22 22:23:44 +0530

    Time: 37 ms (24.01%), Space: 19.8 MB (9.45%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -5,11 +5,13 @@ public:
         int ret=INT_MIN;
         for(int num:nums){
             m[num]++;
+            if(m[num]>nums.size()/2)
+                ret = num;
         }
-        for( auto [k,v]:m)
-        {
-            ret = (v>nums.size()/2)?k:ret;
-        }
+        // for( auto [k,v]:m)
+        // {
+        //     ret = (v>nums.size()/2)?k:ret;
+        // }
         return ret;
     }
 };
