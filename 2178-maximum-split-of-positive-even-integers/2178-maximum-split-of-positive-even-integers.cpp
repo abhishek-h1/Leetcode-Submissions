31e7fcce644448c19ded521da6e0fe33711be8a5
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-02-27 04:53:54 +0530

    Time: 258 ms (31.90%), Space: 40.5 MB (85.09%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<long long> maximumEvenSplit(long long finalSum) {
-        int x=2;
-        vector<long long> v;
+        vector<long long>v;
         if(finalSum%2)
             return v;
-        while(finalSum>=x){
-            finalSum-=x;
+        int x=2;
+        
+        while(finalSum>=x)
+        {
             v.push_back(x);
+            finalSum-=x;
             x+=2;
         }
-        if(finalSum>0)
-        {
-            v[v.size()-1]+=finalSum;
-        }
+        v[v.size()-1]+=finalSum;
         return v;
         
     }
