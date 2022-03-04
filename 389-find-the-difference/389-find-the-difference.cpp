faccba8001cc260591b1bf4638b811ae9bcf4ac9
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-03-04 18:45:34 +0530

    Time: 8 ms (18.38%), Space: 6.9 MB (8.25%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         map<char,int>m;
         for(int i=0;i<t.length()-1;i++)
         {
