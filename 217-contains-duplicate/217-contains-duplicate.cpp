71192e07e8b9e879a437b02be92f5cfbe068c12b
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-06-16 17:00:46 +0530

    Time: 195 ms (10.18%), Space: 52 MB (37.75%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-       map<int,int> m;
+        map<int,int>m;
+        
         for(int i=0;i<nums.size();i++)
         {
             m[nums[i]]++;
-            // cout<<m[nums
             if(m[nums[i]]>=2)
+            {
                 return true;
+            }
         }
         return false;
         
