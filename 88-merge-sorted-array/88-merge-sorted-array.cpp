4b785abdde47adb17cd426d0c8a693224ad5b94b
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-06-17 23:42:03 +0530

    Time: 10 ms (7.61%), Space: 9.2 MB (29.24%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,58 +1,35 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        vector<int> v;
-        
-        int i=0,j=0;
-        if(!nums2.empty())
+        vector<int> nums3;
+        // int s=(m<n)? m:n;
+        int j=0,k=0;
+        while(1)
         {
-            
-            while(i<m && j<n)
+            if(j>=m || k>=n)
+                break;
+            if(nums1[j]<=nums2[k])
             {
-                if(nums1[i]<nums2[j])
-                {
-                    v.push_back(nums1[i]);
-                    i++;
-                }
-                else if(nums1[i]>nums2[j])
-                {
-                    v.push_back(nums2[j]);
-                    j++;
-                }
-                else
-                {
-                    v.push_back(nums1[i]);
-                    v.push_back(nums2[j]);
-                    i++;
-                    j++;
-                }
+                nums3.push_back(nums1[j]);
+                j++;
             }
-            if(i<m)
+            else
             {
-                while(i<m)
-                {
-                    v.push_back(nums1[i]);
-                    i++;
-                }
+                nums3.push_back(nums2[k]);
+                k++;
             }
-
-            if(j<n)
-            {
-                while(j<n)
-                {
-                    v.push_back(nums2[j]);
-                    j++;
-                }
-            }
-            nums1.clear();
-
-            for(int k=0;k<m+n;k++)
-            {
-                nums1.push_back(v[k]);
-            }
-
         }
-        
+        while(j<m)
+        {
+            nums3.push_back(nums1[j]);
+            j++;
+        }
+        while(k<n)
+        {
+            nums3.push_back(nums2[k]);
+            k++;
+        }
+        nums1.clear();
+        nums1=nums3;
     }
 };
