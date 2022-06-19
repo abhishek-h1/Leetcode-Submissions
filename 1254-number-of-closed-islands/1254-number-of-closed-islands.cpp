b117e0d950042fbd8f7d532af9d293b757729ae3
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-06-19 20:03:06 +0530

    Time: 15 ms (74.45%), Space: 9.5 MB (66.35%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &grid, int i ,int j)
+    void dfs(vector<vector<int>>&grid, int r, int c)
     {
-        if(j<0||i<0||i>=grid.size()||j>=grid[0].size()||grid[i][j]==1)
+        if(r<0 or c<0 or r>=grid.size() or c>=grid[0].size() or grid[r][c]!=0)
             return;
-        grid[i][j]=1;
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+        grid[r][c]=2;
         
-        
+        dfs(grid,r-1,c);
+        dfs(grid,r+1,c);
+        dfs(grid,r,c-1);
+        dfs(grid,r,c+1);
     }
     int closedIsland(vector<vector<int>>& grid) {
-        for(int i=0;i<grid[0].size();i++)
-        {
-            if(grid[0][i]==0)
-                dfs(grid,0,i);
-            if(grid[grid.size()-1][i]==0)
-                dfs(grid,grid.size()-1,i);
-        }
         for(int i=0;i<grid.size();i++)
         {
-            if(grid[i][0]==0)
-                dfs(grid,i,0);
-            if(grid[i][grid[0].size()-1]==0)
-                dfs(grid,i,grid[0].size()-1);
+            for(int j=0;j<grid[i].size();j++)
+            {
+                if((i*j==0 or i==grid.size()-1 or j==grid[i].size()-1) and grid[i][j]==0)
+                    dfs(grid,i,j);
+            }
         }
         
         int ans=0;
         for(int i=0;i<grid.size();i++)
         {
-            for(int j=0;j<grid[0].size();j++)
+            for(int j=0;j<grid[i].size();j++)
             {
                 if(grid[i][j]==0)
                 {
-                    dfs(grid, i, j);
+                    dfs(grid,i,j);
                     ans++;
                 }
             }
