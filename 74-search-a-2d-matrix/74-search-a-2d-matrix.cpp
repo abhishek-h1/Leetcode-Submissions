ac88b32a76286aaccf6591ecb2a2dd3f6cb6f4f5
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-06-21 19:22:49 +0530

    Time: 10 ms (23.67%), Space: 9.3 MB (95.19%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,68 +1,34 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int l=0,r=matrix.size()-1;
-        if(matrix.size()==0)
-            return false;
-        int mid = l+((r-l)/2);
+        int m=matrix.size();
         int n=matrix[0].size();
+        int l=0,r=m-1;
+        
         while(l<=r)
         {
-            mid = l+((r-l)/2);
-            if(target>=matrix[mid][0] && target <=matrix[mid][n-1])
+            int mid=l+((r-l)>>1);
+            
+            if(target>=matrix[mid][0] and target<=matrix[mid][n-1])
             {
-                l=0;
-                r=n-1;
-                int p=mid;
-                mid=l+((r-l)/2);
-                while(l<=r)
+                int l1=0,r1=n-1;
+                
+                while(l1<=r1)
                 {
-                    mid=l+((r-l)/2);
-                    if(matrix[p][mid]==target)
-                    {
+                    int mi=l1+((r1-l1)>>1);
+                    if(target==matrix[mid][mi])
                         return true;
-                    }
-                    else if(matrix[p][mid]>target)
-                    {
-                        r=mid-1;
-                    }
+                    if(target>matrix[mid][mi])
+                        l1=mi+1;
                     else
-                        l=mid+1;
+                        r1=mi-1;
                 }
-                return false;
             }
-            else if(target<matrix[mid][0])
-            {
-                r=mid-1;
-            }
-            else
-            {
+            if(target>matrix[mid][n-1])
                 l=mid+1;
-            }
+            else
+                r=mid-1;
         }
         return false;
     }
 };
-
-
-
-// if(r==1)
-        // {
-        //     r=n-1;
-        //     mid=l+((r-l)>>2);
-        //     while(l<=r)
-        //     {
-        //         mid=l+((r-l)>>1);
-        //         if(matrix[0][mid]==target)
-        //         {
-        //             return true;
-        //         }
-        //         else if(matrix[0][mid]>target)
-        //         {
-        //             r=mid-1;
-        //         }
-        //         else
-        //             l=mid+1;
-        //     }
-        //     return false;
-        // }
