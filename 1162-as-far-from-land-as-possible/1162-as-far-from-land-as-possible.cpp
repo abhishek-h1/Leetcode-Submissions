087eff9ac39f75136936bc7869dea9d300bf27e5
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-06-21 19:46:37 +0530

    Time: 72 ms (86.51%), Space: 19.7 MB (83.72%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,49 +1,47 @@
 class Solution {
+private:
+    vector<int>x{0,0,-1,1};
+    vector<int>y{-1,1,0,0};
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        queue<pair<int,int>> q;
+        int n=grid.size();
         
-        for(int i=0;i<grid.size();i++)
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<grid[i].size();j++)
+            for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==1)
                     q.push({i,j});
             }
         }
+        if(q.size() == n*n or q.size()==0)
+            return -1;
         int ans=0;
+        
         while(!q.empty())
         {
+            int sz=q.size();
             ans++;
-            int n=q.size();
-            for(int i=0;i<n;i++)
+            while(sz--)
             {
-                int r=q.front().first;
-                int c=q.front().second;
+                int cx=q.front().first;
+                int cy=q.front().second;
                 q.pop();
-                
-                if(r>0 && grid[r-1][c]==0)
+                for(int i=0;i<4;i++)
                 {
-                    grid[r-1][c]=1;
-                    q.push({r-1,c});
-                }
-                if(c>0 && grid[r][c-1]==0)
-                {
-                    grid[r][c-1]=1;
-                    q.push({r,c-1});
-                }
-                if(r+1<grid.size() && grid[r+1][c]==0)
-                {
-                    grid[r+1][c]=1;
-                    q.push({r+1,c});
-                }
-                if(c+1<grid[0].size() && grid[r][c+1]==0)
-                {
-                    grid[r][c+1]=1;
-                    q.push({r,c+1});
+                    int nx=cx+x[i];
+                    int ny=cy+y[i];
+                    
+                    if(nx>=0 and ny>=0 and nx<n and ny<n and grid[nx][ny]==0)
+                    {
+                        q.push({nx,ny});
+                        grid[nx][ny]=1;
+                    }
                 }
             }
         }
-        return (ans<=1)? -1: ans-1;
+        ans--;
+        return ans? ans:-1;
     }
 };
