8ac65e1020b697c424863911e4403f4843d205aa
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-06-22 19:30:41 +0530

    Time: 80 ms (35.69%), Space: 17.5 MB (87.35%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,79 +1,54 @@
 class Solution {
 public:
-    vector<int>x={0,0,-1,1};
-    vector<int>y={1,-1,0,0};
     
-    bool safe(int i, int j, int r, int c)
+    void dfs(vector<vector<int>> &mat, int r, int c,int prev, vector<vector<bool>>&ocean)
     {
-        if(i<0 || j<0 || j>=c || i>=r)
-            return false;
-        return true;
-    }
-    
-    void bfs(vector<vector<int>> &grid, queue<pair<int,int>> &q, vector<vector<bool>>& vis)
-    {
-        int r=grid.size();
-        int c=grid[0].size();
+        int m=mat.size();
+        int n=mat[0].size();
         
-        while(!q.empty())
-        {
-            auto cur=q.front();
-            q.pop();
-            vis[cur.first][cur.second]=true;
-            
-            for(int i=0;i<4;i++)
-            {
-                int nx = cur.first+x[i];
-                int ny = cur.second+y[i];
-                
-                if(safe(nx,ny,r,c) and grid[nx][ny]>=grid[cur.first][cur.second] and vis[nx][ny]==false)
-                {
-                    q.push({nx,ny});
-                    vis[nx][ny]=true;
-                }
-            }
-            
-            
-        }
+        if(r<0 or c<0 or r>=m or c>=n)
+            return;
+        if(ocean[r][c])
+            return;
+        if(mat[r][c]<prev)
+            return;
+        ocean[r][c]=true;
+        
+        dfs(mat,r+1,c,mat[r][c],ocean);
+        dfs(mat,r-1,c,mat[r][c],ocean);
+        dfs(mat,r,c+1,mat[r][c],ocean);
+        dfs(mat,r,c-1,mat[r][c],ocean);
     }
     
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         
-        vector<vector<bool>> pac(heights.size(), vector<bool>(heights[0].size(),false));
-        vector<vector<bool>> atl(heights.size(), vector<bool>(heights[0].size(),false));
+        int m=heights.size();
+        int n=heights[0].size();
+        vector<vector<bool>> pacific(m, vector<bool>(n,false));
+        vector<vector<bool>> atlantic(m, vector<bool>(n,false));
         
-        queue<pair<int,int>>pq,aq;
         
-        //Pushing columns coordinates that are next to pac and atl respectively
-        for(int i=0;i<heights[0].size();i++)
+        for(int i=0;i<m;i++)
         {
-            pq.push({0,i});
-            pac[0][i]=true;
-            aq.push({heights.size()-1,i});
-            atl[heights.size()-1][i]=true;
-        }
-        //Pushing row coors=dinates that are close to pac and atl resp
-        for(int i=0;i<heights.size()-1;i++)
-        {
-            pq.push({i+1,0});
-            pac[i+1][0]=true;
-            aq.push({i,heights[0].size()-1});
-            atl[i][heights[0].size()-1]=true;
+            dfs(heights,i,0,INT_MIN,pacific);
+            dfs(heights,i,n-1,INT_MIN,atlantic);
         }
         
-        bfs(heights,pq,pac);
-        bfs(heights,aq,atl);
         
-        vector<vector<int>>res;
-        for(int i=0;i<heights.size();i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<heights[0].size();j++)
+            dfs(heights,0,i,INT_MIN,pacific);
+            dfs(heights,m-1,i,INT_MIN,atlantic);
+        }
+        vector<vector<int>> ans;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
             {
-                if(pac[i][j]==true && atl[i][j]==true)
-                    res.push_back({i,j});
+                if(pacific[i][j] && atlantic[i][j])
+                    ans.push_back({i,j});
             }
         }
-        return res;
-        
+        return ans;
     }
 };
