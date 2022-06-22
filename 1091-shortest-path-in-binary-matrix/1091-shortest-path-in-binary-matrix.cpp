adb4c4b12e6b94b164bd001473fc9e958eac769f
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-06-22 22:01:46 +0530

    Time: 81 ms (71.89%), Space: 19.3 MB (73.19%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,51 +1,36 @@
 class Solution {
+    private:
+    vector<int>x{1,-1,0,0,1,1,-1,-1};
+    vector<int>y{0,0,1,-1,1,-1,1,-1};
 public:
-    
-    vector<int>x={0,0,1,-1,1,-1,1,-1};
-    vector<int>y={1,-1,0,0,1,-1,-1,1};
-    
-    bool safe(int i,int j,int n)
-    {
-        if(i<0||j<0||i>=n||j>=n)
-            return false;
-        return true;
-    }
-    
-    
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-        if(grid[0][0]==1 || grid[n-1][n-1]==1)
+        if(grid[0][0]==1 or grid[n-1][n-1]==1)
             return -1;
         queue<pair<int,int>>q;
-        vector<vector<bool>> vis(n,vector<bool>(n,false));
-        
         q.push({0,0});
-        vis[0][0]=true;
         grid[0][0]=1;
+        
         while(!q.empty())
         {
-            int s = q.size();
-            
-            
-                int cx = q.front().first;
-                int cy = q.front().second;
-                q.pop();
+            int cx=q.front().first;
+            int cy=q.front().second;
+            q.pop();
+            for(int i=0;i<8;i++)
+            {
+                int nx=cx+x[i];
+                int ny=cy+y[i];
                 
-                
-                for(int i=0;i<8;i++)
-                {
-                    int nx = cx + x[i];
-                    int ny = cy + y[i];
-                    // cout<<nx<<" "<<ny<<endl;
-                    if(safe(nx,ny,n) and !vis[nx][ny] and grid[nx][ny]==0)
-                    {
-                        grid[nx][ny]=grid[cx][cy]+1;
-                        // cout<<grid[nx][ny]<<endl;
-                        q.push({nx,ny});
-                        vis[nx][ny]=true;
-                    }
-                }
+                if(nx<0 or ny<0 or nx>=n or ny>=n or grid[nx][ny]!=0)
+                    continue;
+                q.push({nx,ny});
+                grid[nx][ny]=grid[cx][cy]+1;
+            }
         }
-        return (grid[n-1][n-1]<=0)? -1:grid[n-1][n-1];
+        
+        
+        if(grid[n-1][n-1]!=0)
+            return grid[n-1][n-1];
+        return -1;
     }
 };
