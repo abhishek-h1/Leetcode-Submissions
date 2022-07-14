4f916f73db6ac3786b3e7a1699e08442e1291124
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-07-14 16:17:09 +0530

    Time: 15 ms (29.23%), Space: 7.1 MB (46.04%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -3,8 +3,10 @@ public:
     bool isIsomorphic(string s, string t) {
         if(s.length()!=t.length())
             return false;
+        
         map<char,char>m;
         set<char>se;
+        
         for(int i=0;i<s.length();i++)
         {
             if(m.find(s[i])==m.end())
@@ -13,13 +15,12 @@ public:
                     return false;
                 m[s[i]]=t[i];
             }
-           
             else
             {
                 if(m[s[i]]!=t[i])
                     return false;
             }
-             se.insert(t[i]);
+            se.insert(t[i]);
         }
         return true;
     }
