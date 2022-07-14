24ebfe94e8828072a264c0ff2db614d52d7dcdbe
Abhishek Hindocha <60708846+abhishek-h1@users.noreply.github.com>
2022-07-14 20:17:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (61.13%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,29 +1,54 @@
+#define sz 26
+
 class Solution {
 public:
-    int strStr(string h, string n) {
-        if(n.size()>h.size())
+    int strStr(string haystack, string needle) {
+        if(needle.empty())
+            return 0;
+        if(needle.length()>haystack.length())
             return -1;
-        int j=0;
-        int ans = -1;
-        for(int i=0;i<h.size();i++)
+        if(haystack==needle)
+            return 0;
+        long n=0,h=0;
+        // long ind=0;
+        
+        for(int i=0;i<needle.size();i++)
         {
-            if(h[i]==n[j])
-            {
-                if(j==0)
-                    ans=i;
-                j++;
-            }
-            else
-            {
-                i=i-j;
-                j=0;
-            }
-            if(j==n.size())
-                return ans;
+            n=(sz*n+(needle[i]-'a'+1))%INT_MAX;
+            h=(sz*h+(haystack[i]-'a'+1))%INT_MAX;
         }
         
+        long p=1;
+        for(int i=0;i<needle.length()-1;i++)
+        {
+            p=(p*sz)%INT_MAX;
+        }
         
-        
+        for(int i=0;i<=haystack.size()-needle.size();i++)
+        {
+            if(n==h)
+            {
+                bool flag=true;
+                for(int j=0;j<needle.size();j++)
+                {
+                    if(needle[j]!=haystack[i+j])
+                    {
+                        flag=false;
+                        break;
+                    }
+                }
+                if(flag)
+                    return i;
+            }
+            if(i<haystack.length()-needle.length())
+            {
+                h=h-(p*(haystack[i]-'a'+1));
+                h=(sz*h+(haystack[i+needle.length()]-'a'+1))%INT_MAX;  
+                if(h<0)
+                    h+=INT_MAX;
+            }
+           
+        }
         return -1;
     }
 };
